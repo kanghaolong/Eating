99ed9323b5ed2ba56260a50d0b9becf9ab8f9ce2
lkjy <lkjy@kanghaolongdemac-mini.local>
2016-05-24 20:20:37 +0800

    1

---
+++ Eating/Eating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eating
+//
+//  Created by lkjy on 16/5/24.
+//  Copyright © 2016年 康康. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("123")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
